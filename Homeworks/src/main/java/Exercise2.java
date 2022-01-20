57bf2a1230eee76ee3f8bb41a3133727cb5d91b6
The_Trolling_stone <adpiero1897@mail.ru>
2022-01-20 23:49:04 +0300

    Final Homework1_Course3 commit (change comment ex2)

--- Homeworks/src/main/java/Exercise2.java
+++ Homeworks/src/main/java/Exercise2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public final class Exercise2<T> {
 
-    //1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+
     public ArrayList<T> arrToArrayList(T[] arr) {
         ArrayList<T> arrayList = new ArrayList<>(arr.length);
         for (T e:arr) {
